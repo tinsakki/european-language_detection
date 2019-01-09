aa3018456d9741bfea67e757950d6f7677ea5f7a
CORP\ajain <ajain@measureone.com>
2019-01-09 11:48:00 +0530

    Commit

--- europe_language_detection/views.py
+++ europe_language_detection/views.py
@@ -27,25 +27,27 @@ labels = {
 }
 a = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 b = os.path.join(a,'europe_language_detection/model.pkl')
-print(b)
 def load():
     with open(b, 'rb') as file:
       vectorizer, clf = pickle.load(file)
     return vectorizer, clf
-
+from textblob import TextBlob
 vectorizer, classifer = load()
 
 def prediction(request):
     ans = ''
     if request.method == 'POST':
+        #POST goes here . is_ajax is must to capture ajax requests. Beginners pit.
         if request.is_ajax():
             email = request.POST.get('email')
             email_input = [email]
             email_input_transformed = vectorizer.transform(email_input)
             prediction = classifer.predict(email_input_transformed)
-            for i, j in labels.items():
-                if i == prediction:
-                    ans = j
-            data = {"email": ans}
-            return JsonResponse(data)
+        for i, j in labels.items():
+            if i == prediction:
+                ans = j
+
+        data = {"email":ans}
+        return JsonResponse(data)
+    #Get goes here
     return render(request,'prediction.html')
